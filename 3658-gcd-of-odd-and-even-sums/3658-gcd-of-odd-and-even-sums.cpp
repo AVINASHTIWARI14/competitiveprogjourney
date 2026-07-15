d560b206725e4f1c28a1a5260111dcf3cc3a1941
AVINASH TIWARI <avinashtiwari2005ws@gmail.com>
2026-07-15 22:13:21 +0530

    Time: 0 ms (100%), Space: 8.5 MB (46.67%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        int sumodd=1;
+        int sumo=1;
+        int sumeven=2;
+        int sume=2;
+        for(int i=0;i<n-1;i++){
+            sumodd+=2;
+            sumo+=sumodd;
+            sumeven+=2;
+            sume+=sumeven;
+        }
+        cout<<sumo;
+        cout<<sume;
+        return gcd(sumo,sume);
+    }
+};
