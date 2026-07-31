62b026532ce6143d0cf44290256fd547330c6377
AVINASH TIWARI <avinashtiwari2005ws@gmail.com>
2026-07-31 16:43:03 +0530

    Time: 2 ms (20.68%), Space: 12.9 MB (5.89%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,9 +1,24 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        sort(arr.begin(),arr.end());
-        vector<int>chawanprash;
-        int count=0;
-        for
+        int freq[2001] = {0};  
+        int used[1001] = {0};   
+
+    
+        for (int i = 0; i < arr.size(); i++) {
+            freq[arr[i] + 1000]++;
+        }
+
+        for (int i = 0; i < 2001; i++) {
+            int f = freq[i];
+            if (f > 0) {
+                if (used[f] == 1) 
+                    return false;
+                used[f] = 1;    
+            }
+        }
+
+        return true;
     }
 };
+
