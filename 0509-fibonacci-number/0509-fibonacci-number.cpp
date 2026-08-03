465a211ccc99e46d44971f783e321dc3dc2427d6
AVINASH TIWARI <avinashtiwari2005ws@gmail.com>
2026-08-03 22:12:42 +0530

    Time: 2 ms (77.89%), Space: 8.2 MB (16.81%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-        vector<int>dp;
+vector<int>dp;
     int fib(int n) {
-        if(dp.empty())
-        dp.assign(n+1,-1);
+        if(dp.empty()){
+            dp.assign(n+1,-1);
+        }
         if(n<=1){
             return n;
         }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
+     if(dp[n]!=-1){
+        return dp[n];
+     }
         return dp[n]=fib(n-1)+fib(n-2);
     }
 };
