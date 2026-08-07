8466ac39355bd51fe7aeb406c699c84ba3e5aae5
AVINASH TIWARI <avinashtiwari2005ws@gmail.com>
2026-08-07 17:02:24 +0530

    Time: 0 ms (100%), Space: 8.6 MB (39.02%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -5,13 +5,19 @@ public:
         if(res.empty()){
             res.assign(n+1,-1);
         }
-    
-        res[0]=1;
-        res[1]=1;
-        for(int i=2;i<res.size();i++){
-            res[i]=res[i-1]+res[i-2];
+        if(n<=1){
+            return 1;
         }
-       return res[n];
+    
+      int  prevp=1;
+      int  prev=1;
+      int curr=0;
+        for(int i=2;i<res.size();i++){
+        curr=prevp+prev;
+        prevp=prev;
+        prev=curr;
+        }
+       return curr;
        
     }
 };
