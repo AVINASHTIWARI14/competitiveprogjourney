5c79fa28580b5655bfeec67591f345d34035c2d1
AVINASH TIWARI <avinashtiwari2005ws@gmail.com>
2026-08-15 16:04:32 +0530

    Time: 0 ms (100%), Space: 8.6 MB (39.06%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
-vector<int>dp;
     int climbStairs(int n) {
-        if(dp.empty())
-        dp.assign(n+1,-1);
-        if(n<=1){
-            return 1;
+        vector<int>dp(n+1);
+       dp[0]=1;
+       dp[1]=1;
+        for(int i=2;i<dp.size();i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        dp[n]=climbStairs(n-1)+climbStairs(n-2);
         return dp[n];
-        
     }
 };
