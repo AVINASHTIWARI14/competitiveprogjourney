d495e7a22534d4a22a9149131d3d2fcce116cdb3
AVINASH TIWARI <avinashtiwari2005ws@gmail.com>
2026-08-19 02:25:20 +0530

    Time: 0 ms (100%), Space: 10.3 MB (72.48%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,21 +1,22 @@
 class Solution {
-    int solve(vector<int>& nums,int i){
-        if(i>nums.size()-1)
-        return 0;
-
+    int check(vector<int>& nums,int i){
+        if(dp.empty())
+        dp.assign(101,-1);
+        if(i>nums.size()-1){
+            return 0;
+        }
         if(dp[i]!=-1){
             return dp[i];
         }
-        int rob=nums[i]+solve( nums,i+2);
-        int skip=solve(nums,i+1);
-        return dp[i]= max(rob,skip);
+        int rob=nums[i]+check(nums,i+2);
+        int skip=check(nums,i+1);
+
+        return dp[i]=max(rob,skip);
     }
 public:
 vector<int>dp;
     int rob(vector<int>& nums) {
-        if(dp.empty()){
-            dp.assign(101,-1);
-        }
-        return solve(nums,0);
+        return check(nums,0);
+        
     }
 };
